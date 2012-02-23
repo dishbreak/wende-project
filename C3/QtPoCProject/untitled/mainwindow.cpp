028965017987dde2d164e75ffd2c4336361e2eb3
dishbreak <dishbreak@gmail.com>
2012-02-23 08:41:42 +0000

    Added some proof-of-concept code to try committing from Qt Creator.

--- C3/QtPoCProject/untitled/mainwindow.cpp
+++ C3/QtPoCProject/untitled/mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //added a line of code for the fun of it.
+    qDebug() << "Hello World! :-)";
+    QMessageBox::information(this, "Hello!", "I can make nifty dialog boxes! :-D", QMessageBox::Ok);
 }
 
 MainWindow::~MainWindow()
