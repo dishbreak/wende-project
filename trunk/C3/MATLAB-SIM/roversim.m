a9965c1500715621e53e9a3bdd58de3dbe8d5c02
heinerb@gmail.com <heinerb@gmail.com@ac4dc56f-8f45-be94-64b8-74421d9049a4>
2012-03-06 03:46:10 +0000

    Updated matlab code

--- trunk/C3/MATLAB-SIM/roversim.m
+++ trunk/C3/MATLAB-SIM/roversim.m
@@ -5,12 +5,18 @@ global dTime;
 global roverPosition;
 global roverBearing;
 
-%Mode
+%Speed
 if roverMode == 0 || roverMode == 1
     roverSpeed = 0.05;
 elseif roverMode == 2
     roverSpeed = 0.2;
+elseif roverMode == 3
+    roverSpeed = .05;
+    roverRadSpeed = 0.01;
+    disp(['Radial Speed = ' num2str(roverRadSpeed*100) ' cm/s']);
 end
+
+%disp(['Speed = ' num2str(roverSpeed*100) ' cm/s']);
 
 %Direction
 xdir = sind(roverBearing);
@@ -20,7 +26,22 @@ roverDirection(2) = ydir/(xdir^2 + ydir^2)^(1/2);
 
 if roverMode == 0 && (roverPosition(1)^2 + roverPosition(2)^2)^(1/2) > 0.7 + 0.65
     %Do Nothing
-else
+elseif roverMode == 0 || roverMode == 1 || roverMode == 2
     roverPosition(1) = roverPosition(1) + roverDirection(1)*roverSpeed*dTime;
     roverPosition(2) = roverPosition(2) + roverDirection(2)*roverSpeed*dTime;
+else %roverMode == 3
+    %Convert X/Y to Polar Coordinates
+    roverPolar(1) = (roverPosition(1)^2 + roverPosition(2)^2)^(1/2);
+    roverPolar(2) = atan2(roverPosition(1),roverPosition(2))*180/pi;
+    if roverPolar(1) < 0.7
+        roverPolar(1) = 0.7;
+        roverPolar(2) = roverBearing;
+    end
+    %Move Rover in Polar Coordinates
+    roverPolar(1) = roverPolar(1) + roverRadSpeed*dTime;
+    roverPolar(2) = roverPolar(2) + (roverSpeed/roverPolar(1))*(180/pi)*dTime;
+    
+    %Convert Polar Back to X/Y Coordinates
+    roverPosition(1) = roverPolar(1)*sind(roverPolar(2));
+    roverPosition(2) = roverPolar(1)*cosd(roverPolar(2));
 end
--- trunk/C3/MATLAB-SIM/wendesim_montecarlo.m
+++ trunk/C3/MATLAB-SIM/wendesim_montecarlo.m
@@ -14,36 +14,40 @@ laserOriginRadius = 3.6; %m
 %Error Budgets
 global cameraMajorError; cameraMajorError = 0.01;   %m (Camera Major Axis Random Error: 1 standard deviation)
 global cameraMinorError; cameraMinorError = 0.01;   %m (Camera Minor Axis Random Error: 1 standard deviation)
-global laserAzError; laserAzError = 0.06;           %deg (Laser Azimuth Random Error: 1 standard deviation)
-global laserElError; laserElError = 0.06;           %deg (Laser Elevation Random Error: 1 standard deviation)
+global laserAzError; laserAzError = 0.06;           %deg (Laser Azimuth Position Random Error: 1 standard deviation)
+global laserElError; laserElError = 0.06;           %deg (Laser Elevation Position Random Error: 1 standard deviation)
 global laserAzBias; laserAzBias = 0.05;             %deg/deg (Laser Azimuth Bias: Degrees of bias per degree moved)
 global laserElBias; laserElBias = 0.05;             %deg/deg (Laser Elevation Bias: Degrees of bias per degree moved)
-global gridlockError; gridlockError = 0;            %deg (Calibration Error: Angle between Laser and Camera)
+global gridlockError; gridlockError = 05;           %deg (Calibration Error: Angle between Laser and Camera)
 global laserOriginError; laserOriginError = 0.10;   %m (Calibration Error: Distance from Center of Playing Field to Laser Origin)
 
 
 %Constants
 global updatePeriod; updatePeriod = .25; %s
-global dTime; dTime = .05; %s
-global cameraOrigin; cameraOrigin = [0,0]; %m
-global laserOrigin; laserOrigin = [0,0]; %m
+global dTime; dTime = .01; %s
+global cameraOrigin; cameraOrigin = [1,-2]; %m
+global laserOrigin; laserOrigin = [-2,3]; %m
 global laserPosition; laserPosition = [0,0];%[(rand-0.5),(rand-0.5)];
+global localLaserVelocity; localLaserVelocity = [0,0];
 global playingFieldRadius; playingFieldRadius = 0.7; %m
 global failureLineRadius; failureLineRadius = 2; %m
 global roverPosition; roverPosition = [0,0]; %m
 global roverBearing; roverBearing = rand*360; %deg
 global playingFieldOrigin; playingFieldOrigin = [0,0]; %m
-global laserHeight; laserHeight = 2; %m
-global laserMaxSpeed; laserMaxSpeed = 20; %deg/s
-global laserMinSpeed; laserMinSpeed = 0; %deg/s
+global systemLatency; systemLatency = 0.1; %s (System latency. Must be a multiple of "dTime")
+global laserHeight; laserHeight = 1.5; %m
+global laserMaxSpeed; laserMaxSpeed = 50; %deg/s
+global laserMaxAcceleration; laserMaxAcceleration = 500; %deg/s^2
+global laserMinSpeed; laserMinSpeed = 5; %deg/s
+global laserMinStep; laserMinStep = .25; %deg
 
 %Initialize
 i = 0;
 global c3RoverFilter; c3RoverFilter.initialized = 0;
-
+commandedAzEl = [0 0];
 
 %WENDE Simulation
-for cameraBearing = 0:angleResolution:360
+for cameraBearing = 360:angleResolution:360
     for laserBearing = 0:angleResolution:360
         for roverBearing = 0:angleResolution:360
             for trial = 1:numTrials
@@ -59,27 +63,27 @@ for cameraBearing = 0:angleResolution:360
                 %Laser
                 laserOrigin = [laserOriginRadius*sind(laserBearing),laserOriginRadius*cosd(laserBearing)];
                 laserPosition = [0,0];
+                localLaserVelocity = [0,0];
                 %**********************************************************
                 display(['Trial Number: ',num2str(i)]);
                 for time = 0:dTime:maxTime
                     %SIMULATION PROCESSING
-                    %**********************************************************
+                    %**********************************************************************
                     %ROVER
                     roversim(roverMode);
                     
                     if mod(time,updatePeriod) == 0
                         %CAMERA
-                        
                         [cameraRoverPosition,cameraLaserPosition] = camerasim;
-                        
-                        
+                    end
+                    if mod(time - systemLatency,updatePeriod) == 0
                         %C3
                         [commandedAzEl] = c3sim(cameraRoverPosition,cameraLaserPosition);
                     end
                     
                     %LASER
                     lasersim(commandedAzEl);
-                    %**********************************************************
+                    %**********************************************************************
                     
                     %Compute Miss Distance
                     missDistance = ((laserPosition(1)-roverPosition(1))^2 + (laserPosition(2)-roverPosition(2))^2)^(1/2);
