984c08b21ec0bed8772461f0cbe3c51fe0caa3cc
rudicio@gmail.com <rudicio@gmail.com@ac4dc56f-8f45-be94-64b8-74421d9049a4>
2012-03-11 21:49:46 +0000

    Added main rover code structure to version control

---
+++ trunk/Rover/main/RoverLaserDetection.h
@@ -0,0 +1,53 @@
+/**
+*	RoverLaserDetection:
+*		-Detect the laser, sample the ADC, etc
+*	Author: Rover Team
+*	Modified: 2/29/2012
+*/
+
+#ifndef RoverLaserDetection_h
+#define RoverLaserDetection_h
+
+#include "Arduino.h"
+
+//states for state machine
+#define INIT_SENSORS		0
+#define SAMPLE_SENSORS	1
+#define DETECT_LASER		2
+#define	ADJUST_TO_LIGHTING	3
+#define DETECTED_LASER	4
+#define SETTLE_FROM_ADJUST  5
+
+#define MIDDLING_SAMPLES  5
+#define ADC_DETECTOR_SAMPLE_RATE  100 //15
+#define ADC_LIGHTING_SAMPLE_RATE  100
+#define SETTLING_CYCLES  100
+
+//Laser Detection data struct
+typedef struct __sensor_data {
+	unsigned int inst_value;		//instantaneous value
+	int current_value;	//current avg'ed value
+	int stable_value;	//current stable value
+	unsigned long total;			//running total
+	unsigned int num_samples;		//number of samples
+	unsigned int samples[MIDDLING_SAMPLES];		//samples
+	unsigned int address;				//location of sensor
+        boolean sampled;         //enough samples?
+        boolean stable;          //stable?
+} sensor_data;
+
+
+
+void DetectionProcessing(void);
+boolean initialize_laser_detection(void);
+boolean DetectLaser(void);
+int adjust_to_light_change(int photodetectorVal);
+void Adjust_Current_Sync(int value);
+void Toggle_Res_On(int pin);
+void Toggle_Res_Off(int pin);
+boolean sample_adc(sensor_data* data, int sample_rate);
+boolean sample_adc(sensor_data* data);
+
+
+
+#endif
