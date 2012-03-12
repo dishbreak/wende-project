f85855750d35ce17ccbe06eee984a99119f8ec18
kyle.kendall15@gmail.com <kyle.kendall15@gmail.com@ac4dc56f-8f45-be94-64b8-74421d9049a4>
2012-03-12 01:05:50 +0000

    Updated Main Camera Subsystem Code, improved MotionTrackingSimple.m, and added three other motion tracking algorithms

--- trunk/Camera/src/Motion_Tracking/MotionTrackingSimple.m
+++ trunk/Camera/src/Motion_Tracking/MotionTrackingSimple.m
@@ -1,5 +1,5 @@
 clc; clear all; close all;
-addpath(genpath('../'))
+addpath(genpath('../../'))
 clear
 close all
 warning off all
@@ -19,42 +19,28 @@ try
     std = uint8(std(double(samples),1,4));
     std = rgb2gray(std);
     tol = 6.*std;
-    numframes=200;
+    numframes=300;
     i=1;
-    bg=rgb2gray(bg);
+    bg=wiener2(rgb2gray(bg),[5 5]);
     disp('Start Moving')
     tic
+    
     while(videoInfo.vid.FramesAcquired<=numframes)
         im = getdata(vid);
-        im=rgb2gray(im);
+        im=wiener2(rgb2gray(im),[5 5]);
         %se = strel('square',5);
         %im = imfill(im,'holes');
         brighterThanBg = (im > bg + tol);
         darkerThanBg = (im < bg - tol);
         Im = (brighterThanBg | darkerThanBg);
-        Im = bwareaopen(Im,2000);
+        Im = bwareaopen(Im,700);
         imshow(Im)
-        %         mFrame(i)=im2frame(uint8(Im),colormap(gray(128)));
-        %         i=i+1;
-%         hold on
-%         stats = regionprops(Im, 'BoundingBox', 'Centroid','Area');
-%         [~,idx]=max([stats.Area]);
-%         if ~isempty(stats)
-%         bb = stats(idx).BoundingBox;
-%         bc = stats(idx).Centroid;
-%         rectangle('Position',bb,'EdgeColor','r','LineWidth',2)
-%         plot(bc(1),bc(2), '-m+')
-%         a=text(bc(1)+15,bc(2), strcat('X: ', num2str(round(bc(1))), '    Y: ', num2str(round(bc(2)))));
-%         set(a, 'FontName', 'Arial', 'FontWeight', 'bold', 'FontSize', 12, 'Color', 'yellow');
-%         end
-%         
-%         hold off
-        mData(i)=getframe;
+        %mData(i)=getframe;
         i=i+1;
         videoInfo.FlushCameraData
     end
     t=toc;
-    procTime=(numframes-10)/t;
+    procTime=(i)/t;
     videoInfo.StopCamera;
     disp(['Total Time = ',num2str(floor((procTime)))])
 catch err
