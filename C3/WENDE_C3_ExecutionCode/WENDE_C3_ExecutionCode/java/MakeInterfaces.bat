b3fab6fd1f5d3b007d5b9a8a26d5cb66e7db0108
heinerb <heinerb@gmail.com>
2012-05-18 01:25:59 +0000

    Added initial laser commands (protobuf)

--- C3/WENDE_C3_ExecutionCode/WENDE_C3_ExecutionCode/java/MakeInterfaces.bat
+++ C3/WENDE_C3_ExecutionCode/WENDE_C3_ExecutionCode/java/MakeInterfaces.bat
@@ -1,1 +1,2 @@
 protoc.exe cameraMsgs.proto --cpp_out=ProtoOutput --java_out=ProtoOutput --python_out=ProtoOutput --error_format=msvs
+protoc.exe laserMsgs.proto --cpp_out=ProtoOutput --java_out=ProtoOutput --python_out=ProtoOutput --error_format=msvs
