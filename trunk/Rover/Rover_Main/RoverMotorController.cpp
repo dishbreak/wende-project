821b7590e7053e242463e090423040b355a90761
rudicio@gmail.com <rudicio@gmail.com@ac4dc56f-8f45-be94-64b8-74421d9049a4>
2012-05-23 01:30:47 +0000

    Integrated Rover Laser detection, UI, and motor control into main rover code.

--- trunk/Rover/Rover_Main/RoverMotorController.cpp
+++ trunk/Rover/Rover_Main/RoverMotorController.cpp
@@ -1,13 +1,33 @@
 #include "RoverMotorController.h"
+#include "RoverInterfaces.h"
+
+//used for encoders
+volatile long LM_count = 0; // Interrupt variables should be volatile (stored in RAM)
+volatile long RM_count = 0;
+long oldLMcount = 0;
+long oldRMcount = 0;
+
 
 /**
 * RoverMotorRoutines: 
 * Description: Main execution to control the motors for the rover
-* @param left, left motor desired speed - derived from the movement mode
-* @param right, right motor desired speed - derived from the movement mode
+* @param left, left motor desired speed data - derived from the movement mode
+* @param right, right motor desired speed data - derived from the movement mode
 */
-void RoverMotorRoutines(int left, int right)
+void RoverMotorRoutines(motor_data* leftMotor, motor_data* rightMotor)
 {
+  static unsigned long lastMilli = 0;
+  
+  if (millis()-lastMilli >= LOOP_TIME) {
+    lastMilli = millis();
+    leftMotor->measuredSpeed = calcLeftMotorSpeed();
+    rightMotor->measuredSpeed = calcRightMotorSpeed();
+
+    leftMotor->PWM_val = calcPWM(leftMotor);
+    rightMotor->PWM_val = calcPWM(leftMotor);
+    setRightMotor(rightMotor->PWM_val);
+    setLeftMotor(leftMotor->PWM_val);
+  }
 }
 
 /**
@@ -16,6 +36,7 @@ void RoverMotorRoutines(int left, int right)
 */
 void incLeftMotor()
 {
+  LM_count++;
 }
 
 /**
@@ -24,4 +45,37 @@ void incLeftMotor()
 */
 void incRightMotor()
 {
+  RM_count++;
+}
+
+/**
+* calcRightMotorSpeed: 
+* Description: Calculate measured speed based on encoder counts
+* @return measured speed
+*/
+int calcRightMotorSpeed()
+{
+  return (RM_count-oldRMcount)*SPEED_MULTIPLIER;
+}
+
+/**
+* calcLeftMotorSpeed: 
+* Description: Calculate measured speed based on encoder counts
+* @return measured speed
+*/
+int calcLeftMotorSpeed()
+{
+  return (LM_count-oldLMcount)*SPEED_MULTIPLIER;
+}
+
+//PWM speed
+int calcPWM(motor_data* motorData)
+{
+  float PIDdelta = 0;
+  int error = 0;
+  error = abs(motorData->targetSpeed)-abs(motorData->measuredSpeed); // calculate error
+  motorData->cumError += error;     // calculate cumulative error
+  PIDdelta = motorData->Kp*error-motorData->Kd*(error-motorData->lastError)+motorData->Ki*motorData->cumError;
+  motorData->lastError = error; // set last error
+  return constrain(motorData->PWM_val+int(PIDdelta),0,255); // constrain the new value to range
 }
--- trunk/Rover/Rover_Main/RoverMotorController.h
+++ trunk/Rover/Rover_Main/RoverMotorController.h
@@ -3,7 +3,7 @@
 *		-Control the rover motors
 *		-Interrupt Service Routines (ISR) for encoders
 *	Author: Rover Team
-*	Modified: 5/16/2012
+*	Modified: 5/22/2012
 */
 
 #ifndef RoverMotorController_h
@@ -11,11 +11,35 @@
 
 #include "Arduino.h"
 
+//defines
+#define LOOP_TIME  100
+#define SPEED_MULTIPLIER  ((1000/(3*53))*(204/LOOP_TIME))
+
+//motor data structure
+//Laser Detection data struct
+typedef struct __motor_data {
+  int PWM_val;
+  int targetSpeed;
+  int measuredSpeed;
+  int cumError;
+  int lastError;
+  float Kp; // proportional gain value
+  float Ki; // integrative gain value
+  float Kd; // derivative gain value  
+} motor_data;
+
 //main rover motor controller routines
-void RoverMotorRoutines(int left, int right);
+void RoverMotorRoutines(motor_data* leftMotor, motor_data* rightMotor);
 
 //left/right encoder ISR
 void incLeftMotor();
 void incRightMotor();
+
+//left/right motor speeds
+int calcRightMotorSpeed();
+int calcLeftMotorSpeed();
+
+//PWM speed
+int calcPWM(motor_data* motorData);
 
 #endif
