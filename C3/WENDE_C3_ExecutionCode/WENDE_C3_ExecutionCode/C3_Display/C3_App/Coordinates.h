ab96939ff72c57f4d10d5552781a29ef58a043d5
dishbreak <dishbreak@gmail.com>
2012-06-02 07:24:24 +0000

    Created Singleton class for Coordinates.

---
+++ C3/WENDE_C3_ExecutionCode/WENDE_C3_ExecutionCode/C3_Display/C3_App/Coordinates.h
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+
+class Coordinates
+{
+public:
+	static Coordinates * GetCoordinatesHandle();
+	void SetNewCoordinatePair(int,int);
+	struct CoordinatePair {
+		int x;
+		int y;
+	};
+	CoordinatePair GetNewCoordinatePair();
+private:
+	Coordinates();
+	~Coordinates();
+	CoordinatePair TranslateCoords(CoordinatePair);
+	CoordinatePair CurWorldCoords;
+	CoordinatePair OldWorldCoords;
+	CoordinatePair PixelCoords;
+	CoordinatePair PixelBounds;
+	CoordinatePair WorldBounds;
+	static Coordinates * coordsObj;
+};
