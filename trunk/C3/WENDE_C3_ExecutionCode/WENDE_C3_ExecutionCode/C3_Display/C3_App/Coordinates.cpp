f0e9c81e6eb02a4c02da9fe3d90f68489a6a12b9
dishbreak@gmail.com <dishbreak@gmail.com@ac4dc56f-8f45-be94-64b8-74421d9049a4>
2012-06-02 07:24:24 +0000

    Created Singleton class for Coordinates.

---
+++ trunk/C3/WENDE_C3_ExecutionCode/WENDE_C3_ExecutionCode/C3_Display/C3_App/Coordinates.cpp
@@ -0,0 +1,61 @@
+#include "stdafx.h"
+#include "Coordinates.h"
+
+//Make sure pointer starts as NULL
+Coordinates * Coordinates::coordsObj = 0;
+
+Coordinates::Coordinates() {
+	//initialize all coordinate pairs
+	Coordinates::CurWorldCoords.x = 0;
+	Coordinates::CurWorldCoords.y = 0;
+	Coordinates::OldWorldCoords.x = 0;
+	Coordinates::OldWorldCoords.y = 0;
+	Coordinates::WorldBounds.x    = 5000;
+	Coordinates::WorldBounds.y    = 5000;
+	Coordinates::PixelBounds.x    = 320;
+	Coordinates::PixelBounds.y    = 320;
+}
+
+Coordinates::~Coordinates() {
+
+}
+
+Coordinates * Coordinates::GetCoordinatesHandle() {
+	if (!coordsObj) {
+		coordsObj = new Coordinates();
+	}
+	return coordsObj;
+}
+
+void Coordinates::SetNewCoordinatePair(int xCoord, int yCoord) {
+	//Only update if the coordinates are new
+	if (xCoord != CurWorldCoords.x && yCoord != CurWorldCoords.y)
+	{
+		//Copy old coords out
+		OldWorldCoords.x = CurWorldCoords.x;
+		OldWorldCoords.y = CurWorldCoords.y;
+		//Bring in new coords
+		CurWorldCoords.x = xCoord;
+		CurWorldCoords.y = yCoord;
+		//Turn the new coords into Pixel Coords
+		PixelCoords = Coordinates::TranslateCoords(CurWorldCoords);
+	}
+	
+}
+
+Coordinates::CoordinatePair Coordinates::GetNewCoordinatePair() {
+	return Coordinates::PixelCoords;
+}
+
+Coordinates::CoordinatePair Coordinates::TranslateCoords(Coordinates::CoordinatePair WorldCoords) {
+	Coordinates::CoordinatePair newCoords;
+	float coordsRatio = 0.0;
+	//Translate X
+	coordsRatio = (float) WorldCoords.x / WorldBounds.x;
+	newCoords.x = coordsRatio * PixelBounds.x;
+	//Translate Y
+	coordsRatio = 0.0;
+	coordsRatio = (float) WorldCoords.y / WorldBounds.y;
+	newCoords.y = coordsRatio * PixelBounds.y;
+	return newCoords;
+}
