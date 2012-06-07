59451d877aaeb1437ae468623e66532b4c28ffab
f9073341@googlemail.com <f9073341@googlemail.com@ac4dc56f-8f45-be94-64b8-74421d9049a4>
2012-06-07 23:50:24 +0000

    Fixed rover acquired indicator

--- trunk/C3/WENDE_C3_ExecutionCode/WENDE_C3_ExecutionCode/C3_Display/C3_App/CNetworkMonitor.cpp
+++ trunk/C3/WENDE_C3_ExecutionCode/WENDE_C3_ExecutionCode/C3_Display/C3_App/CNetworkMonitor.cpp
@@ -294,12 +294,15 @@ UINT WINAPI TrackThread (LPVOID pParam)
 					freshCoordinates[i]->x = sTrackMessage.Tracks[i].X;
 					freshCoordinates[i]->y = sTrackMessage.Tracks[i].Y;
 				}
-				//x = sTrackMessage.Tracks->X; //x += 1;
-				//y = sTrackMessage.Tracks->Y; //y += 1;
 
 				////get a handle to the CDisplayManager
                 CDisplayManager ^dispman = CDisplayManager::getCDisplayManager();
 				dispman->Update_Rover_PPI_Position(freshCoordinates, sTrackMessage.ValidTracks);
+
+				if(sTrackMessage.ValidTracks >= 1)
+					dispman->Update_Rover_Acquired_Indicator(1);
+				else
+					dispman->Update_Rover_Acquired_Indicator(0);
 
 				//This check is already built into DisplayManager
 				//if(x >= 1 || y >= 1) dispman->Update_Rover_Acquired_Indicator(1);
