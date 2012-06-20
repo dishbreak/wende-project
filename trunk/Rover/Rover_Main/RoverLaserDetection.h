cf9751af5710391644c3b66a3d5e0d3777e8abbd
rudicio@gmail.com <rudicio@gmail.com@ac4dc56f-8f45-be94-64b8-74421d9049a4>
2012-06-20 02:52:24 +0000

    -Added potential fix to rover "wiggling" movement (thought to be associated with loop time delay between motor adjustments -Simplified/reworked laser detection threshold checks -Removed any unnecessary multiplication/division as it bogs the micro controller down

--- trunk/Rover/Rover_Main/RoverLaserDetection.h
+++ trunk/Rover/Rover_Main/RoverLaserDetection.h
@@ -12,28 +12,20 @@
 
 //states for state machine
 #define INIT_SENSORS		0
-#define CALIBRATE_SENSORS       1
-#define LASER_DETECTION         2
-#define SAMPLE_SENSORS	        3
-#define DETECT_LASER		4
-#define	ADJUST_TO_LIGHTING	5
-#define DETECTED_LASER	        6
-#define SETTLE_FROM_ADJUST      7
-#define RESAMPLE_AFTER_ADJUST   8
+#define LASER_DETECTION         1
 
 #define MAX_SAMPLES  1000
 
 #define SENSOR_OFFSET_BITS      8
 #define MAX_SENSOR_OFFSET       (2^SENSOR_OFFSET_BITS)-1
-#define SETTLING_CYCLES  100  //capacitors removed
-#define SETTLING_TIME  2      //2ms to settle...sigh
+#define SETTLING_TIME  100  //used for serial shifter
 
-#define DETECTION_LOW           200 //was 60
+#define DETECTION_LOW           180 //was 60
 #define DETECTION_HIGH          500 //was 500
 #define AMBIENT_LIGHT_LIMIT_POS 75  //False alarms all the way down to 40
 #define AMBIENT_LIGHT_LIMIT_NEG 75  //False alarms all the way down to 40
 #define AMBIENT_DETECTION_LIMIT_NEG 20  //False alarms all the way down to 20
-#define HIT_TIME                3  //was 10
+#define HIT_TIME                15  //was 10
   
 boolean DetectionProcessing(void);
 int adjust_to_light_change(int photodetectorVal);
